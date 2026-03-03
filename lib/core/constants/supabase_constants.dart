5d0976f9ccd4c726a407df7bc318a962deb5bbc6
Habitra Dev <habitra@app.dev>
2026-03-03 13:09:57 +0000

    🔐 Supabase bağlantısı yapılandırıldı ve güvenlik düzeltmeleri
    - Supabase URL ve anon key eklendi
    - RLS eksik tablolara (categories, presets, achievements, prompts) eklendi
    - Function search_path güvenlik düzeltmesi
    - Leaderboard view security_invoker ile güncellendi

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,7 +1,8 @@
 class SupabaseConstants {
-  // TODO: Supabase projenizi oluşturduktan sonra bu değerleri güncelleyin
-  static const String url = 'https://YOUR_PROJECT_ID.supabase.co';
-  static const String anonKey = 'YOUR_ANON_KEY';
+  static const String url = 'https://nttnbvgqfakpxfuefpyn.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dG5idmdxZmFrcHhmdWVmcHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NDEyMzYsImV4cCI6MjA4ODExNzIzNn0.3qVPIgFB8-k40J4Tr6lmWZig-c4nWDdwbKueJHTn7vs';
+  
+  // TODO: Google Cloud Console'dan OAuth Client ID'leri alıp buraya ekleyin
   static const String googleClientId = 'YOUR_GOOGLE_CLIENT_ID';
   static const String googleClientIdIOS = 'YOUR_GOOGLE_IOS_CLIENT_ID';
 }
