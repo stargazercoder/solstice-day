100b36c19aa5c0b0ac520b16438e073c6fa16932
Habitra Dev <habitra@app.dev>
2026-03-04 12:23:25 +0000

    🐛 Fix: Türkçe locale initialization eklendi
    - initializeDateFormatting('tr_TR') main'de çağrılıyor
    - Takvim ekranındaki locale hatası düzeltildi

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'core/constants/supabase_constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('tr_TR', null);
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
